15213be6144e33481dab070a23883c51a9b8d9a4
John Ralls <jralls@ceridwen.us>
2021-08-21 11:58:29 -0700

    Fix sed invalid byte sequence errors.
    These were caused by passing graphics files through sed because the
    repository has more than just png files.

--- install.sh
+++ install.sh
@@ -5,7 +5,12 @@ if [ "x$JHBUILD_PREFIX" = x ]; then
     exit 1
 fi
 
-for f in `find share -type f ! -name *.png`; do
+for f in `find share -type f -name catalog -or -name *.xml -or -name *.mod`; do
     mkdir -p "$DESTDIR/$JHBUILD_PREFIX/`dirname $f`"
     cat $f | sed -e "s,/usr/share,$JHBUILD_PREFIX/share,g" > "$DESTDIR/$JHBUILD_PREFIX/$f"
+done
+
+for f in `find share -type f ! \( -name catalog -or -name *.xml -or -name *.mod \)`; do
+    mkdir -p "$DESTDIR/$JHBUILD_PREFIX/`dirname $f`"
+    cp $f "$DESTDIR/$JHBUILD_PREFIX/$f"
 done
